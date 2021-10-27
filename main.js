1540e2476ce552960b5dc3837040a1e73d830134
Andrius <andriuss0316@gmail.com>
2021-10-27 12:25:22 +0300

    20211027

---
+++ main.js
@@ -0,0 +1,388 @@
+
+let pc = 0;
+let randomm = 0;
+
+let pcNumberr = 0;      // pcNumberr = pcNumberr + 1; pcNumber.innerText = pcNumberr;
+let yourNumberr = 0;    // yourNumberr = yourNumberr + 1; yourNumber.innerText = yourNumberr;
+let drawNumberr = 0;    // drawNumberr = drawNumberr + 1; drawNumber.innerText = drawNumberr;
+
+
+// 1 - rock; 2 - paper; 3 - scissors
+
+
+const rock = document.querySelector('.rock');
+const paper = document.querySelector('.paper');
+const scissors = document.querySelector('.scissors');
+
+const rezultatas = document.querySelector('.rezultatas');
+
+const random = document.querySelector('.random');
+
+const pcNumber = document.querySelector('.pcNumber');
+const yourNumber = document.querySelector('.yourNumber');
+const drawNumber = document.querySelector('.drawNumber');
+
+
+
+pcNumber.innerText = pcNumberr; 
+yourNumber.innerText = yourNumberr; 
+drawNumber.innerText = drawNumberr; 
+
+
+{ document.querySelector(".pc").style.display = "none"; }
+{ document.querySelector(".you").style.display = "none"; }
+{ document.querySelector(".loading").style.display = "none"; }
+
+
+
+var imgPC = document.querySelector(".pc");
+var imgYOU = document.querySelector(".you");
+
+
+// pcNumber.InnerText = 
+
+
+function rockClick() {
+    randomm = 1;
+    pc = (Math.floor(Math.random() * 3) + 1);
+
+    rezultatas.innerText = '';
+    { document.querySelector(".loading").style.display = "block"; }
+    { document.querySelector(".pc").style.display = "none"; }
+    { document.querySelector(".you").style.display = "none"; }
+
+    setTimeout(function () {
+        if (randomm === 1) {
+            imgYOU = document.querySelector(".you");
+            imgYOU.src = "./pic/rock.png";
+            imgYOU.height = 200;
+        } else if (randomm === 2) {
+            imgYOU = document.querySelector(".you");
+            imgYOU.src = "./pic/paper.png";
+            imgYOU.height = 200;
+        } else if (randomm === 3) {
+            imgYOU = document.querySelector(".you");
+            imgYOU.src = "./pic/scissors.png";
+            imgYOU.height = 200;
+        }
+
+        if (pc === 1) {
+            imgPC = document.querySelector(".pc");
+            imgPC.src = "./pic/pc/rockPC.png";
+            imgPC.height = 200;
+        } else if (pc === 2) {
+            imgPC = document.querySelector(".pc");
+            imgPC.src = "./pic/pc/paperPC.png";
+            imgPC.height = 200;
+        } else if (pc === 3) {
+            imgPC = document.querySelector(".pc");
+            imgPC.src = "./pic/pc/scissorsPC.png";
+            imgPC.height = 200;
+        }
+
+        if (pc === 1 && randomm === 1) {
+            rezultatas.innerText = "draw";
+            drawNumberr = drawNumberr + 1; 
+            drawNumber.innerText = drawNumberr;
+        } else if (pc === 1 && randomm === 2) {
+            rezultatas.innerText = "you win";
+            yourNumberr = yourNumberr + 1; 
+            yourNumber.innerText = yourNumberr;
+        } else if (pc === 1 && randomm === 3) {
+            rezultatas.innerText = "pc win";
+            pcNumberr = pcNumberr + 1; 
+            pcNumber.innerText = pcNumberr;
+        } else if (pc === 2 && randomm === 1) {
+            rezultatas.innerText = "pc win";
+            pcNumberr = pcNumberr + 1; 
+            pcNumber.innerText = pcNumberr;
+        } else if (pc === 2 && randomm === 2) {
+            rezultatas.innerText = "draw";
+            drawNumberr = drawNumberr + 1; 
+            drawNumber.innerText = drawNumberr;
+        } else if (pc === 2 && randomm === 3) {
+            rezultatas.innerText = "you win";
+            yourNumberr = yourNumberr + 1; 
+            yourNumber.innerText = yourNumberr;
+        } else if (pc === 3 && randomm === 1) {
+            rezultatas.innerText = "you win";
+            yourNumberr = yourNumberr + 1; 
+            yourNumber.innerText = yourNumberr;
+        } else if (pc === 3 && randomm === 2) {
+            rezultatas.innerText = "pc win";
+            pcNumberr = pcNumberr + 1; 
+            pcNumber.innerText = pcNumberr;
+        } else if (pc === 3 && randomm === 3) {
+            rezultatas.innerText = "draw";
+            drawNumberr = drawNumberr + 1; 
+            drawNumber.innerText = drawNumberr;
+        }
+
+        { document.querySelector(".pc").style.display = "block"; }
+        { document.querySelector(".you").style.display = "block"; }
+
+        { document.querySelector(".loading").style.display = "none"; }
+    }, 500);
+}
+
+function paperClick() {
+    randomm = 2;
+    pc = (Math.floor(Math.random() * 3) + 1);
+
+    rezultatas.innerText = '';
+    { document.querySelector(".loading").style.display = "block"; }
+    { document.querySelector(".pc").style.display = "none"; }
+    { document.querySelector(".you").style.display = "none"; }
+
+    setTimeout(function () {
+        if (randomm === 1) {
+            imgYOU = document.querySelector(".you");
+            imgYOU.src = "./pic/rock.png";
+            imgYOU.height = 200;
+        } else if (randomm === 2) {
+            imgYOU = document.querySelector(".you");
+            imgYOU.src = "./pic/paper.png";
+            imgYOU.height = 200;
+        } else if (randomm === 3) {
+            imgYOU = document.querySelector(".you");
+            imgYOU.src = "./pic/scissors.png";
+            imgYOU.height = 200;
+        }
+
+        if (pc === 1) {
+            imgPC = document.querySelector(".pc");
+            imgPC.src = "./pic/pc/rockPC.png";
+            imgPC.height = 200;
+        } else if (pc === 2) {
+            imgPC = document.querySelector(".pc");
+            imgPC.src = "./pic/pc/paperPC.png";
+            imgPC.height = 200;
+        } else if (pc === 3) {
+            imgPC = document.querySelector(".pc");
+            imgPC.src = "./pic/pc/scissorsPC.png";
+            imgPC.height = 200;
+        }
+
+        if (pc === 1 && randomm === 1) {
+            rezultatas.innerText = "draw";
+            drawNumberr = drawNumberr + 1; 
+            drawNumber.innerText = drawNumberr;
+        } else if (pc === 1 && randomm === 2) {
+            rezultatas.innerText = "you win";
+            yourNumberr = yourNumberr + 1; 
+            yourNumber.innerText = yourNumberr;
+        } else if (pc === 1 && randomm === 3) {
+            rezultatas.innerText = "pc win";
+            pcNumberr = pcNumberr + 1; 
+            pcNumber.innerText = pcNumberr;
+        } else if (pc === 2 && randomm === 1) {
+            rezultatas.innerText = "pc win";
+            pcNumberr = pcNumberr + 1; 
+            pcNumber.innerText = pcNumberr;
+        } else if (pc === 2 && randomm === 2) {
+            rezultatas.innerText = "draw";
+            drawNumberr = drawNumberr + 1; 
+            drawNumber.innerText = drawNumberr;
+        } else if (pc === 2 && randomm === 3) {
+            rezultatas.innerText = "you win";
+            yourNumberr = yourNumberr + 1; 
+            yourNumber.innerText = yourNumberr;
+        } else if (pc === 3 && randomm === 1) {
+            rezultatas.innerText = "you win";
+            yourNumberr = yourNumberr + 1; 
+            yourNumber.innerText = yourNumberr;
+        } else if (pc === 3 && randomm === 2) {
+            rezultatas.innerText = "pc win";
+            pcNumberr = pcNumberr + 1; 
+            pcNumber.innerText = pcNumberr;
+        } else if (pc === 3 && randomm === 3) {
+            rezultatas.innerText = "draw";
+            drawNumberr = drawNumberr + 1; 
+            drawNumber.innerText = drawNumberr;
+        }
+
+        { document.querySelector(".pc").style.display = "block"; }
+        { document.querySelector(".you").style.display = "block"; }
+
+        { document.querySelector(".loading").style.display = "none"; }
+    }, 500);
+}
+
+function scissorsClick() {
+    randomm = 3;
+    pc = (Math.floor(Math.random() * 3) + 1);
+
+    rezultatas.innerText = '';
+    { document.querySelector(".loading").style.display = "block"; }
+    { document.querySelector(".pc").style.display = "none"; }
+    { document.querySelector(".you").style.display = "none"; }
+
+    setTimeout(function () {
+        if (randomm === 1) {
+            imgYOU = document.querySelector(".you");
+            imgYOU.src = "./pic/rock.png";
+            imgYOU.height = 200;
+        } else if (randomm === 2) {
+            imgYOU = document.querySelector(".you");
+            imgYOU.src = "./pic/paper.png";
+            imgYOU.height = 200;
+        } else if (randomm === 3) {
+            imgYOU = document.querySelector(".you");
+            imgYOU.src = "./pic/scissors.png";
+            imgYOU.height = 200;
+        }
+
+        if (pc === 1) {
+            imgPC = document.querySelector(".pc");
+            imgPC.src = "./pic/pc/rockPC.png";
+            imgPC.height = 200;
+        } else if (pc === 2) {
+            imgPC = document.querySelector(".pc");
+            imgPC.src = "./pic/pc/paperPC.png";
+            imgPC.height = 200;
+        } else if (pc === 3) {
+            imgPC = document.querySelector(".pc");
+            imgPC.src = "./pic/pc/scissorsPC.png";
+            imgPC.height = 200;
+        }
+
+        if (pc === 1 && randomm === 1) {
+            rezultatas.innerText = "draw";
+            drawNumberr = drawNumberr + 1; 
+            drawNumber.innerText = drawNumberr;
+        } else if (pc === 1 && randomm === 2) {
+            rezultatas.innerText = "you win";
+            yourNumberr = yourNumberr + 1; 
+            yourNumber.innerText = yourNumberr;
+        } else if (pc === 1 && randomm === 3) {
+            rezultatas.innerText = "pc win";
+            pcNumberr = pcNumberr + 1; 
+            pcNumber.innerText = pcNumberr;
+        } else if (pc === 2 && randomm === 1) {
+            rezultatas.innerText = "pc win";
+            pcNumberr = pcNumberr + 1; 
+            pcNumber.innerText = pcNumberr;
+        } else if (pc === 2 && randomm === 2) {
+            rezultatas.innerText = "draw";
+            drawNumberr = drawNumberr + 1; 
+            drawNumber.innerText = drawNumberr;
+        } else if (pc === 2 && randomm === 3) {
+            rezultatas.innerText = "you win";
+            yourNumberr = yourNumberr + 1; 
+            yourNumber.innerText = yourNumberr;
+        } else if (pc === 3 && randomm === 1) {
+            rezultatas.innerText = "you win";
+            yourNumberr = yourNumberr + 1; 
+            yourNumber.innerText = yourNumberr;
+        } else if (pc === 3 && randomm === 2) {
+            rezultatas.innerText = "pc win";
+            pcNumberr = pcNumberr + 1; 
+            pcNumber.innerText = pcNumberr;
+        } else if (pc === 3 && randomm === 3) {
+            rezultatas.innerText = "draw";
+            drawNumberr = drawNumberr + 1; 
+            drawNumber.innerText = drawNumberr;
+        }
+
+        { document.querySelector(".pc").style.display = "block"; }
+        { document.querySelector(".you").style.display = "block"; }
+
+        { document.querySelector(".loading").style.display = "none"; }
+    }, 500);
+}
+
+function randomClick() {
+    randomm = (Math.floor(Math.random() * 3) + 1);
+    pc = (Math.floor(Math.random() * 3) + 1);
+
+    rezultatas.innerText = '';
+    { document.querySelector(".loading").style.display = "block"; }
+    { document.querySelector(".pc").style.display = "none"; }
+    { document.querySelector(".you").style.display = "none"; }
+
+    setTimeout(function () {
+        if (randomm === 1) {
+            imgYOU = document.querySelector(".you");
+            imgYOU.src = "./pic/rock.png";
+            imgYOU.height = 200;
+        } else if (randomm === 2) {
+            imgYOU = document.querySelector(".you");
+            imgYOU.src = "./pic/paper.png";
+            imgYOU.height = 200;
+        } else if (randomm === 3) {
+            imgYOU = document.querySelector(".you");
+            imgYOU.src = "./pic/scissors.png";
+            imgYOU.height = 200;
+        }
+
+        if (pc === 1) {
+            imgPC = document.querySelector(".pc");
+            imgPC.src = "./pic/pc/rockPC.png";
+            imgPC.height = 200;
+        } else if (pc === 2) {
+            imgPC = document.querySelector(".pc");
+            imgPC.src = "./pic/pc/paperPC.png";
+            imgPC.height = 200;
+        } else if (pc === 3) {
+            imgPC = document.querySelector(".pc");
+            imgPC.src = "./pic/pc/scissorsPC.png";
+            imgPC.height = 200;
+        }
+
+        if (pc === 1 && randomm === 1) {
+            rezultatas.innerText = "draw";
+            drawNumberr = drawNumberr + 1; 
+            drawNumber.innerText = drawNumberr;
+        } else if (pc === 1 && randomm === 2) {
+            rezultatas.innerText = "you win";
+            yourNumberr = yourNumberr + 1; 
+            yourNumber.innerText = yourNumberr;
+        } else if (pc === 1 && randomm === 3) {
+            rezultatas.innerText = "pc win";
+            pcNumberr = pcNumberr + 1; 
+            pcNumber.innerText = pcNumberr;
+        } else if (pc === 2 && randomm === 1) {
+            rezultatas.innerText = "pc win";
+            pcNumberr = pcNumberr + 1; 
+            pcNumber.innerText = pcNumberr;
+        } else if (pc === 2 && randomm === 2) {
+            rezultatas.innerText = "draw";
+            drawNumberr = drawNumberr + 1; 
+            drawNumber.innerText = drawNumberr;
+        } else if (pc === 2 && randomm === 3) {
+            rezultatas.innerText = "you win";
+            yourNumberr = yourNumberr + 1; 
+            yourNumber.innerText = yourNumberr;
+        } else if (pc === 3 && randomm === 1) {
+            rezultatas.innerText = "you win";
+            yourNumberr = yourNumberr + 1; 
+            yourNumber.innerText = yourNumberr;
+        } else if (pc === 3 && randomm === 2) {
+            rezultatas.innerText = "pc win";
+            pcNumberr = pcNumberr + 1; 
+            pcNumber.innerText = pcNumberr;
+        } else if (pc === 3 && randomm === 3) {
+            rezultatas.innerText = "draw";
+            drawNumberr = drawNumberr + 1; 
+            drawNumber.innerText = drawNumberr;
+        }
+
+        { document.querySelector(".pc").style.display = "block"; }
+        { document.querySelector(".you").style.display = "block"; }
+
+        { document.querySelector(".loading").style.display = "none"; }
+    }, 500);
+
+
+}
+
+// 1 - rock; 2 - paper; 3 - scissors
+
+
+rock.addEventListener('click', rockClick);
+
+paper.addEventListener('click', paperClick);
+
+scissors.addEventListener('click', scissorsClick);
+
+random.addEventListener('click', randomClick);
